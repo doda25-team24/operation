69f70e71f562d2160c53235a4847d64053057707
Sara Cortez <131759998+SarinhaCortez@users.noreply.github.com>
2025-12-29 16:14:24 +0000

    monitoring works for model-service

--- setup_newprofile_issue.sh
+++ setup_newprofile_issue.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-# Script for Minikube newprofile setup, issue that happened on the mac of one of the team members(Sara Cortez)
+# Script for Minikube np setup, issue that happened on the mac of one of the team members(Sara Cortez)
 #builds on local minikube, not on A2
 
-PROFILE="newprofile"
 
-
-echo "Starting Minikube with profile '$PROFILE'..."
-minikube start -p $PROFILE --driver=docker --memory=4600MB --cpus=2 --bootstrapper=kubeadm
-minikube -p newprofile addons enable ingress
+echo "Starting Minikube with profile 'np'..."
+minikube start -p np --driver=docker --memory=4600MB --cpus=2 --bootstrapper=kubeadm
+minikube -p np addons enable ingress
 
 # run this every time you open a new terminal
 echo "Setting Docker environment for Minikube..."
-eval $(minikube -p $PROFILE docker-env)
+eval $(minikube -p np docker-env)
+
+#i did not need to load images into minikube w np on my mac, but if you do, uncomment below
+# echo "Loading images into Minikube..."
+# minikube -p np image load sms-model-service:latest
+# minikube -p np image load sms-checker-app:latest
 
 echo "Building Docker images..."
 docker build -t sms-model-service:latest -f ../model-service/Dockerfile ../model-service
 docker build -t sms-checker-app:latest -f ../app/Dockerfile ../app
 
-#i did not need to load images into minikube w newprofile on my mac, but if you do, uncomment below
-# echo "Loading images into Minikube..."
-# minikube -p $PROFILE image load sms-model-service:latest
-# minikube -p $PROFILE image load sms-checker-app:latest
-
 echo "Mounting shared folder..."
-nohup minikube  -p $PROFILE mount ../model-service/output:/model-service/output > /tmp/minikube-mount.log 2>&1 & 
+nohup minikube  -p np mount ../model-service/output:/model-service/output > /tmp/minikube-mount.log 2>&1 & 
+nohup sudo minikube tunnel -p np  > /tmp/tunnel.log 2>&1 & 
 
 echo "install prometheus"
 helm repo add prom-repo https://prometheus-community.github.io/helm-charts
@@ -43,14 +42,15 @@ echo "Starting a 30 second wait to ensure pods are running...... "; sleep 30; ec
 
 echo "Waiting for pods to be ready..."
 
-kubectl --context=$PROFILE get pods
+kubectl --context=np get pods
 
 echo "Waiting for svc to be ready..."
-kubectl --context=$PROFILE get svc 
+kubectl --context=np get svc 
 
 echo "Waiting for pvc to be ready..."
-kubectl --context=$PROFILE get pvc
+kubectl --context=np get pvc
 
 echo "Waiting for pv to be ready..."
-kubectl --context=$PROFILE get pv
+kubectl --context=np get pv
+
 
